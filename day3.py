ab779d9b6e94855e24977538c404da3e98261ed5
Ankita Singh <pingme@ankita@gmail.com>
2026-01-15 23:39:28 +0530

    Added python practice files

---
+++ day3.py
@@ -0,0 +1,5 @@
+def calculate(a, b):
+    return a + b, a - b, a * b
+
+sum_, diff, prod = calculate(8, 3)
+print(sum_, diff, prod)
--- match case.py
+++
@@ -1,8 +0,0 @@
-x=int(input("enter a number between 1 to 5: "))
-match x:
-    case _if x!=90:
-        print(x,"case is 4")
-        case _if x!=80:
-            print('x,"is not 80")
-                  case:
-                  print(x)
